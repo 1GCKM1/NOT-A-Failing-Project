485efee0da2603680d1ccababe9f30377014d676
slowak <machina4785@gmail.com>
2017-07-20 16:13:27 +0700

    initial server

--- server.js
+++ server.js
@@ -2,7 +2,17 @@
 * @Author: slowak
 * @Date:   2017-07-20 15:40:43
 * @Last Modified by:   slowak
-* @Last Modified time: 2017-07-20 15:40:43
+* @Last Modified time: 2017-07-20 16:05:18
 */
 
 'use strict';
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello');
+});
+
+var server = app.listen(3000, function() {
+	console.log("Server running at http://localhost:" + server.address().port);
+});
